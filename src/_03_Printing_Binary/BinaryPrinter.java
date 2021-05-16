5fa2a4ec4feb7932d7155f8bd6ca61b64b5e8da0
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2021-05-16 12:57:10 -0700

    binary printer ip

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -32,20 +32,34 @@ public class BinaryPrinter {
 		// Use bit shifting and masking (&) to save the first
 		// 8 bits of s in one byte, and the second 8 bits of
 		// s in the other byte
-		
+		for(int i = 1; i < 8; i++) {
+			int firstBit = s % 10;
+			int shiftBit = s >> 7;
+			int num = firstBit & 1;
+		}
+		for(int i = 8; i < 16; i++) {
+			int firstBit = s % 10;
+			int shiftBit = s >> 7;
+			int num = firstBit & 1;
+		}
 		// Call printByteBinary twice using the two bytes
 		// Make sure they are in the correct order
+		//b1.printByteBinary();
+		//b2.printByteBinary();
 	}
 	
 	public void printIntBinary(int i) {
 		// Create 2 short variables
-		
+		short a;
+		short b;
 		// Use bit shifting and masking (&) to save the first
 		// 16 bits of i in one short, and the second 16 bits of
 		// i in the other short
 		
 		// Call printShortBinary twice using the two short variables
 		// Make sure they are in the correct order
+		//a.printShortBinary();
+		//b.printShortBinary();
 	}
 	
 	public void printLongBinary(long l) {
