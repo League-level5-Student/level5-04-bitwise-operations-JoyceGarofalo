28cd961cb61cb38d4e432ec1b2aafbe2ee36fbd3
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2021-06-13 12:56:14 -0700

    base 64 decoder ip

--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -3,6 +3,7 @@ package _04_Base64_Decoder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Base64Decoder {
 	/*
@@ -52,25 +53,92 @@ public class Base64Decoder {
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		String char1 = Integer.toBinaryString(s.charAt(0));
-		String char2 = Integer.toBinaryString(s.charAt(1));
-		String char3 = Integer.toBinaryString(s.charAt(2));
-		String char4 = Integer.toBinaryString(s.charAt(3));
+		System.out.println(s);
+		char c0 = s.charAt(0);
+		char c1 = s.charAt(1);
+		char c2 = s.charAt(2);
+		char c3 = s.charAt(3);
+
+		ArrayList<Integer> first6 = new ArrayList<Integer>();
+		ArrayList<Integer> second6 = new ArrayList<Integer>();
+		ArrayList<Integer> third6 = new ArrayList<Integer>();
+		ArrayList<Integer> fourth6 = new ArrayList<Integer>();
 
 		
-		byte c1 = (byte) char1.length(); //number of bytes
-		byte c2 = (byte) char2.length();
-		byte c3 = (byte) char3.length();
-		byte c4 = (byte) char4.length();
-		byte[] arr = {c1, c2, c3, c4};	
+		int result = convertBase64Char(c0);
+		for(int i = 5; i > -1; i--) {
+			int numShifted = result >> i;
+			int numOnes = numShifted%10;
+			numOnes = numOnes & 1;
+			//System.out.print(numOnes);
+			first6.add(numOnes);
+		}
 		
-		return arr;
+		//System.out.print(first6);
+		//System.out.print("hi");
+		
+		int result_1 = convertBase64Char(c1);
+		for(int i = 5; i > -1; i--) {
+			int numShifted = result_1 >> i;
+			int numOnes = numShifted%10;
+			numOnes = numOnes & 1;
+			//System.out.print(numOnes);
+			second6.add(numOnes);
+		}
+		
+		int result_2 = convertBase64Char(c2);
+		for(int i = 5; i > -1; i--) {
+			int numShifted = result_2 >> i;
+			int numOnes = numShifted%10;
+			numOnes = numOnes & 1;
+			//System.out.print(numOnes);
+			third6.add(numOnes);
+		}
+		
+		int result_3 = convertBase64Char(c3);
+		for(int i = 6; i > -1; i--) {
+			int numShifted = result_3 >> i;
+			int numOnes = numShifted%10;
+			numOnes = numOnes & 1;
+			//System.out.print(numOnes);
+			fourth6.add(numOnes);
+		}
+		
+		ArrayList<Integer> totalArray = new ArrayList<Integer>();
+		totalArray.addAll(first6);
+		totalArray.addAll(second6);
+		totalArray.addAll(third6);
+		totalArray.addAll(fourth6);
+		//System.out.print(totalArray);
+		//24 values
+		
+		
+		int[] first8 = new int[8];
+		int[] second8 = new int[8];
+		int[] third8 = new int[8];
+
+		for(int i = 0; i < 8; i++) {
+			first8[i] = totalArray.get(i);
+			second8[i] = totalArray.get(i+8);
+			third8[i] = totalArray.get(i+16);
+		}
+		
+		//SO FAR: first8, second8, third8 have values in binary. Need to change these back into decimal
+		//put decimal values into array of 3 values and return array
+		
+		//Collect all values into one array. Split into groups of eight.
+		//Each group turn it back into a number. Return array of those numbers
+		
+		return null;
 	}	//doesn't work
 	
 	
 	//3. Complete this method so that it takes in a string of any length
 	//   and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
+		
+		
+		
 		return null;
 	}
 }
